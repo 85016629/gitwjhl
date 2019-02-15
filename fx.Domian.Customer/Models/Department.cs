a9502a34fffd599405fccaf217fd3d5f6b8271aa
fx.dwc <85016629@qq.com>
2019-02-15 17:32:22 +0800

    20190215

---
+++ fx.Domian.Customer/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using fx.Domain.core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fx.Domain.CustomerContext.Models
+{
+    public class Department : AggregateRoot<int>
+    {        
+        public string DepartmentName { get; set; }
+        public IList<Department> SubDepartments { get; set; }
+    }
+}
---
+++ fx.Infra.Data.SqlServer/Repositories/User/DepartmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using fx.Domain.CustomerContext.Models;
+using fx.Domain.CustomerContext.RepositoryInterface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace fx.Infra.Data.SqlServer.Repositories.User
+{
+    public class DepartmentRepository : BaseRepository<Department, int>, IDepartmentRepository
+    {
+    }
+}

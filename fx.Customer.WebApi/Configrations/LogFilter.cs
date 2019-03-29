f31755fa51cbc99e1f51e0fb000492215d045ef4
fx.dwc <85016629@qq.com>
2019-03-29 18:05:17 +0800

    20190329

---
+++ fx.Customer.WebApi/Configrations/LogFilter.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fx.Customer.WebApi.Configrations
+{
+    public class LogFilter : IActionFilter, IAsyncActionFilter
+    {
+        /// <summary>
+        /// 执行以后发送的事情。
+        /// </summary>
+        /// <param name="context"></param>
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+        }
+
+        /// <summary>
+        /// 执行以前发生的事情。
+        /// </summary>
+        /// <param name="context"></param>
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
